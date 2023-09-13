df1f056fd65e839e2188b9dc89df049526009f4a
letsmailvjkumar <letsmailvjkumar@gmail.com>
2023-09-13 13:36:05 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,50 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React,{useState} from 'react'
+
+const App = () => {
+    const [todos, setTodos] = useState([])
+    const [count, setCount] = useState(0)
+    const [skill,setSkill] = useState([])
+    const [newSkill, setNewSkill] = useState('')
+
+    const handleAddSkill = () => {
+        if (newSkill.length > 5) {
+          setSkill([...skill, newSkill]);
+          setNewSkill('');
+        } else {
+          alert('Please enter more than 5 characters.');
+        }
+      };
+
+
+  return (
+    <div>
+        <h2>React.useMemo</h2>
+        <h3>My todos</h3>
+        <p>{todos}</p>
+        <button onClick={()=>setTodos('New Todo')}>Add Todo</button>
+        <hr></hr>
+        <span>Count: {count}</span>
+        <button onClick={()=>setCount(count+1)}>+</button>
+        <h3>Expensive Calculation</h3>
+        <p>1000000000</p>
+        <hr></hr>
+        <hr></hr>
+        <h3>React.memo</h3>
+        <input type='text' value={newSkill} onChange={(e)=>setNewSkill(e.target.value)}/>
+        <button onClick={handleAddSkill}>Add Skill</button>
+        <ul>
+        {
+            skill.map((item,index)=>{
+                return(
+                    <li key={index}>{item}</li>
+                )
+            }
+            )
+        }
+        </ul>
+    </div>
+  )
+}
+
+export default App
